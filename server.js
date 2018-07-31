d3523db5e022bf7f022aeb0a68d2d21e786f41ab
halfb00t <halfb00t@gmail.com>
2018-07-31 20:41:35 +0600

    Added https redirect

--- server.js
+++ server.js
@@ -2,9 +2,20 @@ var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
-app.use(serveStatic(__dirname + "/dist"));
 
 var port = process.env.PORT || 5000;
+
+app.enable('trust proxy');
+
+app.use (function (req, res, next) {
+        if (req.secure) {
+                next();
+        } else {
+                res.redirect('https://' + req.headers.host + req.url);
+        }
+});
+
+app.use(serveStatic(__dirname + "/dist"));
 
 app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname + '/dist/index.html'));
